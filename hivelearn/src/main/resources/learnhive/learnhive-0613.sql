5e84bed837d9cb30b27c08ee60150552084cf16a
cycloneboy <xuanfeng1992@gmail.com>
2020-06-14 16:03:27 +0800

    hive编程指南第六章和第七章sql

--- hivelearn/src/main/resources/learnhive/learnhive-0613.sql
+++ hivelearn/src/main/resources/learnhive/learnhive-0613.sql
@@ -301,3 +301,432 @@ insert overwrite local directory '/tmp/il_employees'
 select *
 where se.country = 'US'
   and se.state = 'IL';
+
+
+
+-------------------------------------------------------------------
+--   第六章 HiveSql 数据查询
+-------------------------------------------------------------------
+
+select name, subordinates
+from learnhive.employees;
+
+select name, deductions
+from learnhive.employees;
+
+select name, address
+from learnhive.employees;
+
+select name, subordinates[0]
+from learnhive.employees;
+
+select name, deductions["State Taxes"]
+from learnhive.employees;
+
+select name, address.city
+from learnhive.employees;
+
+select *
+from learnhive.stocks
+limit 10;
+
+show create table learnhive.stocks;
+
+load data local inpath '/home/sl/learnhive/prog-hive-1st-ed-data/data/stocks/stocks.csv'
+into table learnhive.stocks;
+
+drop table learnhive.stocks;
+
+create
+external table if not exists stocks(
+    `exchange` string,
+    symbol string,
+    ymd string,
+    price_open float,
+    price_high float,
+    price_low float,
+    price_close float,
+    volume int,
+    price_adj_close float
+)
+row format delimited fields terminated by ','
+location '/data/stocks';
+
+select *
+from learnhive.stocks
+limit 10;
+
+select symbol, `price.*`
+from learnhive.stocks;
+
+select upper(name),
+       salary,
+       deductions["Federal Taxes"],
+       round(salary * (1 - deductions["Federal Taxes"]))
+from learnhive.employees;
+
+select count(*), avg(salary)
+from learnhive.employees;
+
+set hive.map.aggr = true;
+
+select count(distinct symbol)
+from learnhive.stocks;
+
+select explode(subordinates) as sub
+from learnhive.employees;
+
+select upper(name),
+       salary,
+       deductions["Federal Taxes"],
+       round(salary * (1 - deductions["Federal Taxes"]))
+from learnhive.employees
+limit 2;
+
+select upper(name),
+       salary,
+       deductions["Federal Taxes"]                       as fed_taxes,
+       round(salary * (1 - deductions["Federal Taxes"])) as salary_minus_fed_taxes
+from learnhive.employees;
+
+from
+(
+    select upper(name)                                       as name,
+           salary,
+           deductions["Federal Taxes"]                       as fed_taxes,
+           round(salary * (1 - deductions["Federal Taxes"])) as salary_minus_fed_taxes
+    from learnhive.employees
+)
+e
+select e.name, e.salary_minus_fed_taxes
+where e.salary_minus_fed_taxes > 70000;
+
+select name,
+       salary,
+       case
+           when salary < 50000.0 then 'low'
+           when salary >= 50000.0 and salary < 70000.0 then 'middle'
+           when salary >= 70000.0 and salary < 100000.0 then 'high'
+           else 'very high'
+           end as bracket
+from learnhive.employees;
+
+select *
+from learnhive.employees;
+select *
+from learnhive.employees
+where country = 'US'
+  and state = 'CA';
+
+set hive.exec.mode.local.auto = true;
+
+
+select upper(name),
+       salary,
+       deductions["Federal Taxes"]                as fed_taxes,
+       salary * (1 - deductions["Federal Taxes"]) as salary_minus_fed_taxes
+from learnhive.employees
+where round(salary * (1 - deductions["Federal Taxes"])) > 70000;
+
+
+select upper(name),
+       salary,
+       deductions["Federal Taxes"]                as fed_taxes,
+       salary * (1 - deductions["Federal Taxes"]) as salary_minus_fed_taxes
+from learnhive.employees
+where salary_minus_fed_taxes > 70000;
+
+select e.*
+from (
+         select upper(name),
+                salary,
+                deductions["Federal Taxes"]                as fed_taxes,
+                salary * (1 - deductions["Federal Taxes"]) as salary_minus_fed_taxes
+         from learnhive.employees) e
+where e.salary_minus_fed_taxes > 70000;
+
+select name, salary, deductions["Federal Taxes"]
+from learnhive.employees;
+
+select name, salary, deductions["Federal Taxes"]
+from learnhive.employees
+where deductions["Federal Taxes"] > 0.2;
+
+select name, salary, deductions["Federal Taxes"]
+from learnhive.employees
+where deductions["Federal Taxes"] > cast(0.2 as float);
+
+
+select name, address.street
+from learnhive.employees
+where address.street like '%Ave.';
+
+select name, address.city
+from learnhive.employees
+where address.city like 'O%';
+
+select name, address.street
+from learnhive.employees
+where address.street like '%Chi%';
+
+select name, address.street
+from learnhive.employees
+where address.street rlike '.*(Chicago|Ontario).*';
+
+select year(ymd), avg(price_close)
+from learnhive.stocks
+where `exchange` = 'NASDAQ'
+  and symbol = 'AAPL'
+group by year(ymd);
+
+select year(ymd), avg(price_close) as avg_price_close
+from learnhive.stocks
+where `exchange` = 'NASDAQ'
+  and symbol = 'AAPL'
+group by year(ymd)
+having avg_price_close > 50.0;
+
+select year, avg_price_close
+from (select year(ymd)        as year,
+             avg(price_close) as avg_price_close
+      from learnhive.stocks
+      where `exchange` = 'NASDAQ'
+        and symbol = 'AAPL'
+      group by year(ymd)
+     ) s2
+where s2.avg_price_close > 50.0;
+
+
+select a.ymd, a.price_close, b.price_close
+from learnhive.stocks as a
+         join learnhive.stocks as b
+              on a.ymd = b.ymd
+where a.symbol = 'AAPL'
+  and b.symbol = 'IBM';
+
+select a.ymd, a.price_close, b.price_close
+from learnhive.stocks as a
+         join learnhive.stocks as b
+              on a.ymd <= b.ymd
+where a.symbol = 'AAPL'
+  and b.symbol = 'IBM';
+
+create
+external table if not exists dividends(
+    ymd string,
+    dividend float
+)
+partitioned by (`exchange` string,symbol string)
+row format delimited fields terminated by ','
+location '/data/dividends';
+
+drop table dividends;
+
+create
+external table if not exists dividends_tmp(
+    `exchange` string,
+    symbol string,
+    ymd string,
+    dividend float
+)
+row format delimited fields terminated by ','
+location '/data/tmp/dividends';
+
+load data local inpath "/home/sl/learnhive/prog-hive-1st-ed-data/data/dividends/dividends.csv"
+into table dividends_tmp;
+
+select *
+from dividends_tmp
+limit 10;
+
+select `exchange`, symbol, count(*)
+from dividends_tmp
+group by `exchange`, symbol;
+
+select *
+from dividends_tmp
+where symbol = 'GE';
+select *
+from dividends_tmp
+where symbol = 'IBM';
+select *
+from dividends_tmp
+where symbol = 'AAPL';
+
+
+from dividends_tmp
+insert into table dividends partition (`exchange`='NASDAQ', symbol='AAPL')
+select ymd, dividend
+where `exchange` = 'NASDAQ'
+  and symbol = 'AAPL'
+insert into table dividends partition (`exchange`='NYSE', symbol='IBM')
+select ymd, dividend
+where `exchange` = 'NYSE'
+  and symbol = 'IBM'
+insert into table dividends partition (`exchange`='NYSE', symbol='GE')
+select ymd, dividend
+where `exchange` = 'NYSE'
+  and symbol = 'GE';
+
+
+select `exchange`, symbol
+from dividends
+group by `exchange`, symbol;
+
+select s.ymd, s.symbol, s.price_close, d.dividend
+from learnhive.stocks s
+         join dividends d on s.ymd = d.ymd and s.symbol = d.symbol
+where s.symbol = 'AAPL';
+
+select a.ymd, a.price_close, b.price_close, c.price_close
+from learnhive.stocks as a
+         join learnhive.stocks as b on a.ymd <= b.ymd
+         join learnhive.stocks as c on a.ymd <= c.ymd
+where a.symbol = 'AAPL'
+  and b.symbol = 'IBM'
+  and c.symbol = 'GE';
+
+set hive.strict.checks.cartesian.product = false;
+set hive.mapred.mode = nostrict;
+
+set hive.strict.checks.cartesian.product = true;
+set hive.mapred.mode = strict;
+
+
+select s.ymd, s.symbol, s.price_close, d.dividend
+from learnhive.stocks s
+         join dividends d on s.ymd = d.ymd and s.symbol = d.symbol
+where s.symbol = 'AAPL';
+
+select s.ymd, s.symbol, s.price_close, d.dividend
+from learnhive.stocks s
+         left outer join dividends d on s.ymd = d.ymd and s.symbol = d.symbol
+where s.symbol = 'AAPL';
+
+select s.ymd, s.symbol, s.price_close, d.dividend
+from learnhive.stocks s
+         left outer join dividends d on s.ymd = d.ymd and s.symbol = d.symbol
+where s.symbol = 'AAPL'
+  and d.`exchange` = 'NASDAQ';
+
+SELECT *
+from learnhive.stocks
+         join learnhive.dividends;
+
+select /* +MAPJOIN(d) */s.ymd, s.symbol, s.price_close, d.dividend
+from learnhive.stocks s
+         join learnhive.dividends d on s.ymd = d.ymd and s.symbol = d.symbol
+where s.symbol = 'AAPL';
+
+set hive.auto.convert.join;
+set hive.auto.convert.join = true;
+
+select s.ymd, s.symbol, s.price_close, d.dividend
+from learnhive.stocks s
+         join learnhive.dividends d on s.ymd = d.ymd and s.symbol = d.symbol
+where s.symbol = 'AAPL';
+
+set hive.optimize.bucketmapJOIN;
+set hive.optimize.bucketmapJOIN = true;
+
+select s.ymd, s.symbol, s.price_close
+from learnhive.stocks s
+order by s.ymd asc, s.symbol desc;
+
+select s.ymd, s.symbol, s.price_close
+from learnhive.stocks s
+where s.symbol = 'AAPL'
+order by s.ymd asc, s.symbol desc;
+
+select s.ymd, s.symbol, s.price_close
+from learnhive.stocks s DISTRIBUTE BY S.symbol
+SORT by s.ymd asc ,s.symbol
+desc;
+
+select s.ymd, s.symbol, s.price_close
+from learnhive.stocks s CLUSTER BY S.symbol;
+
+
+create table numbers
+(
+    number int
+);
+
+describe formatted numbers;
+
+load data local inpath '/home/sl/numbers.txt'
+into table numbers;
+
+select *
+from numbers;
+
+select *
+from learnhive.numbers tablesample ( bucket 3 out of 10
+on rand()) s;
+
+select *
+from learnhive.numbers tablesample ( bucket 3 out of 10
+on number) s;
+
+select *
+from learnhive.numbers tablesample ( bucket 1 out of 2
+on number) s;
+
+select *
+from learnhive.numbers tablesample ( bucket 2 out of 2
+on number) s;
+
+create table numbers_bucketed
+(
+    number int
+) clustered by (number) into 3 buckets;
+
+set hive.enforce.bucketing = true;
+
+insert overwrite table learnhive.numbers_bucketed
+select *
+from learnhive.numbers;
+
+select *
+from learnhive.numbers_bucketed tablesample ( bucket 2 out of 3
+on number) s;
+
+
+-------------------------------------------------------------------
+--   第7章 HiveSql 视图
+-------------------------------------------------------------------
+show create table learnhive.employees;
+
+select *
+from learnhive.employees;
+
+create view employees_deduction_F(name, deduction) as
+select e.name, e.deductions["Federal Taxes"]
+from learnhive.employees e;
+
+create view employees_deduction_S(name, deduction) as
+select e.name, e.deductions["State Taxes"]
+from learnhive.employees e;
+
+create view if
+            not exists employees_deduction_S(name,deduction)
+            comment "test view"
+            tblproperties ('creator'='sl')
+as
+select e.name, e.deductions["State Taxes"]
+from learnhive.employees e;
+
+drop view if exists learnhive.employees_deduction_S;
+
+select explode(deductions) as (dname,deduction)
+from learnhive.employees;
+
+select *
+from employees_deduction_F;
+
+select *
+from employees_deduction_S;
+
+describe formatted learnhive.employees_deduction_S;
+
+alter view learnhive.employees_deduction_S set tblproperties ('create_at'='2020-06-14');
